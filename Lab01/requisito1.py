53076bdb201de192c377d189704d35c356995ee4
MariaClaudia1328 <mariaclaudia8.0@hotmail.com>
2021-03-15 23:07:35 -0300

    Primeira leitura com tabela de simbolos

--- Lab01/requisito1.py
+++ Lab01/requisito1.py
@@ -1,5 +1,10 @@
+import re #bib para usar regex
 
-#Identificacao dos registradores pela máscara, retorna um inteiro correspondente ao registrador
+# --o que não sei resolver ainda--
+    # quando o label termina de ser montado 
+    # no caso de constantes muito grandes
+
+#Identificacao dos registradores pela máscara, retorna um inteiro correspondente ao registrador (usado na hora da montagem)
 def number_reg(mask):
     if(mask == '$0' or mask == '$zero'):return 0
     if(mask == '$1' or mask == '$at'):return 1
@@ -34,26 +39,48 @@ def number_reg(mask):
     if(mask == '$30' or mask == '$fp'):return 30
     if(mask == '$31' or mask == '$ra'):return 31           
 
+# --primeira leitura--
+    # ler linha a linha
+    # cada linha eu acrescento 32 bits ao endereço 
+    # se for encontrado um label (nome que termina com : ) seu endereço e nome é guardado num dicionario diferente (symbol table)
+
 #Leitura do arquivo de entrada .asm
 entrada = open('requisito2.asm','r')
 
 #Confere se arquivo foi aberto e começa o processo
 if entrada.mode == 'r':
-    # AQUI COMECA O MONTADOR
-    # --primeira leitura--
-        # ler linha a linha
-        # cada linha eu acrescento 32 bits ao endereço 
-        # se for encontrado um label (nome que termina com : ) seu endereço e nome é guardado num dicionario diferente (symbol table)
-    # --segunda leitura--
-        # ler linha a linha 
-        # no caso de encontrar instrução com apenas registradores: armazenar num dicionario --> 'endereço': 'instrução em bits'
-        # no caso de encontrar label: confere se o label existe e monta
-            # no caso de label dentro da instrução --> procura se existe, seu endereço e monta
-    # --o que não sei resolver ainda--
-        # quando o label termina de ser montado 
-        # no caso de constantes muito grandes
+    contents = entrada.readlines() # le linha a linha  
+    address = 0 # endereço (int)
+    reg = re.compile('\w+:') # regex que é utilizada para identificar os labels
+    symbolTable = {} #dicionario da tabela de simbolos das labels
+    for i in contents:
+        address += 32 # cada instrução tem 32 bits
+        matched = reg.match(i) # busca label em cada linha
+        if matched: # se achou, pega a string, retira o : e adciona no dicionario com chave = address e valor = string
+            label = matched.group()
+            label = re.sub(':','',label)
+            symbolTable[address] =  label
 
-    contents = entrada.readlines()        
+# Fechar arquivo          
+entrada.close()
+
+# ------ fim primeira parte -----------
+
+# --segunda leitura--
+    # ler linha a linha 
+    # no caso de encontrar instrução com apenas registradores: armazenar num dicionario --> 'endereço': 'instrução em bits'
+    # no caso de encontrar label: confere se o label existe e monta
+        # no caso de label dentro da instrução --> procura se existe, seu endereço e monta 
+
+#Leitura do arquivo de entrada .asm       
+entrada = open('requisito2.asm','r')
+
+# aqui começa a montar a linguagem de máquina
+if entrada.mode == 'r':
+    conteudo = entrada.readlines()
+    print(conteudo)
+
+# ------ fim segunda parte -----------
 
 # Escrita no arquivo das saídas de .data e .text
 saida01 = open('saida_data.mif','w')
